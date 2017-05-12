52e6e13f52d209417ad2a60a2e43281978ca07df
Chris Sanders <scoot@Chriss-MacBook-Air.local>
2017-05-12 17:14:18 -0500

    linkContainer

---
+++ src/Link.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './css/Link.css';
+
+class Link extends Component {
+  render() {
+    return (<a href="" className="Link"> <img alt="image"></img> </a>)
+  }
+}
+
+export default Link;
---
+++ src/LinkContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Link from './Link.js';
+
+class LinkContainer extends Component {
+  render() {
+    return(<div className="LinkContainer">
+            <Link />
+            <Link />
+            <Link />
+           </div>)
+  }
+}
+
+export default LinkContainer
